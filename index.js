ce56e280853c513e58675b38e13859c3c8644739
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-12-30 23:02:22 +0100

    all save & pull

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express'); 
 // const app = express.express();
 const app = express();
+const cors = require('cors'); 
 
+app.use(cors())
 app.get('/',(req,res) => {
     // console.log('req,res',req,res);
     res.send('Hello');
     // res.send(req);
-    
 })
+
+
+
 
 // app.listen('3005');
 app.listen(3005,() => {
